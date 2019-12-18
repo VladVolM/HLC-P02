4feae293d88823249a1e6638f1cf20403cb93b5f
alumno <alumno@pc18.iris.majuelo.es>
2019-12-18 19:53:21 +0100

    Añadido el prototipo a unir apartado y slideshow background, ademas funcion de lectura de texto

--- SlideShow/javaInicio.js
+++ SlideShow/javaInicio.js
@@ -8,11 +8,32 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
+
   var slides = document.getElementsByClassName("slideshow");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
 		slides[i].style.display = "none";
-	}
+  }
   	slides[slideIndex-1].style.display = "block";
 }
+
+function readTextFile(file)
+{
+    var x=document.getElementById("descarga");
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                x.innerHTML=allText;
+            }
+        }
+    }
+    rawFile.send(null);
+}
+  
